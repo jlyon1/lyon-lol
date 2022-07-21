27c03fd5fb73e1051383f691e614aaf358e8d381
Joseph Lyon <joeylyon1@gmail.com>
2022-07-20 20:47:50 -0400

    add new stuff

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    entry: './public/app.js',
+    output: {
+      path: __dirname + '/dist',
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [{
+        test: /\.css$/,
+        use: [
+            {
+                loader: 'style-loader'
+            },
+            {
+                loader: 'css-loader'
+            }
+        ]
+      }]
+    }
+  }
